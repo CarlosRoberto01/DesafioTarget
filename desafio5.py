0c3989bfa40e64568441fbafc95cf70fbd21ef3a
Carlos Roberto <carloos.bezerra@hotmail.com>
2024-03-18 10:49:09 -0300

    Finalizando o desafio

--- desafio5.py
+++ desafio5.py
@@ -6,3 +6,10 @@
 #a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
 
 #b) Evite usar funções prontas, como, por exemplo, reverse;
+
+def inverte_string(palavra):
+    return palavra[::-1]
+
+palavra = input(f"Escreva uma palavra e o programa ira invverter todos os caracteres: ")
+palavra_invertida = inverte_string(palavra)
+print(palavra_invertida)
